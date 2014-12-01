353d60e0e2ec81c28dbb3932052d680a9f505d92
Jeronimo Backes <jbax@univocity.com>
2014-12-01 21:19:09 +1030

    updating to use univocity-output-tester

--- src/test/java/com/univocity/examples/Example.java
+++ src/test/java/com/univocity/examples/Example.java
@@ -5,21 +5,21 @@
  ******************************************************************************/
 package com.univocity.examples;
 
-import static org.testng.Assert.*;
-
-import java.io.*;
-import java.util.*;
-
 import com.univocity.api.*;
 import com.univocity.api.common.*;
 import com.univocity.api.config.*;
 import com.univocity.api.entity.text.csv.*;
 import com.univocity.api.entity.text.fixed.*;
+import com.univocity.test.*;
 
 /**
  * Just a parent class for all examples provided, with basic entity configurations and some utility methods to validate outputs.
  */
-abstract class Example {
+abstract class Example extends OutputTester {
+
+	public Example() {
+		super("examples/expectedOutputs/", "UTF-8");
+	}
 
 	// To make testing easier and clearer, all data written in the example test cases will end up in a String
 	protected StringWriterProvider foodGroupOutput;
@@ -99,111 +99,5 @@ abstract class Example {
 		//Registers this engine configuration.
 		Univocity.registerEngine(engineConfig);
 		//##CODE_END
-	}
-
-	public void printAndValidate(String output) {
-		// TODO: If you are modifying the code in the examples to
-		// get to know how things work, just set the validate argument false.
-		printAndValidateOutput(true, output);
-	}
-
-	/**
-	 * Appends some content to the output and adds a newline at the end.
-	 * @param out the output to have content appended to.
-	 * @param contents the contents to be appended
-	 */
-	public void println(StringBuilder out, Object contents) {
-		out.append(contents).append('\n');
-	}
-
-	/**
-	 * Appends a newline to the output
-	 * @param out the output to have a newline appended
-	 */
-	public void println(StringBuilder out) {
-		out.append('\n');
-	}
-
-	/**
-	 * Prints the result to the standard output and validates it against the expected output
-	 * stored in /examples/expectedOutputs/{test_class_name}/{test_method_name}
-	 *
-	 * @param output the result of the test case to be validated against the expected output of the test.
-	 */
-	public void printAndValidate(StringBuilder output) {
-		printAndValidate(output.toString());
-	}
-
-	/**
-	 * Finds out the example being executed and compares the output against
-	 * the expected output in /src/test/resources/examples/expectedOutputs
-	 * @param validate flag to indicate whether the output should be validated
-	 * @param producedOutput the output produced by an example
-	 */
-	private void printAndValidateOutput(boolean validate, String producedOutput) {
-		StackTraceElement[] stack = Thread.currentThread().getStackTrace();
-		for (StackTraceElement element : stack) {
-			String className = element.getClassName();
-
-			if (className.endsWith("." + Example.class.getSimpleName())) {
-				continue;
-			}
-
-			if (className.startsWith("com.univocity.examples")) {
-				String method = element.getMethodName();
-
-				if (method.startsWith("print")) {
-					continue;
-				}
-
-				className = className.substring(className.lastIndexOf('.') + 1, className.length());
-
-				System.out.println("\n=== Output of example: " + className + "." + method + " ===");
-				System.out.println(producedOutput);
-
-				if (validate) {
-					validateExampleOutput(className, method, producedOutput);
-				}
-
-				return;
-			}
-		}
-
-		fail("Could not load file with expected output");
-	}
-
-	private void validateExampleOutput(String className, String testMethod, String producedOutput) {
-		String path = "/examples/expectedOutputs/" + className + "/" + testMethod;
-
-		InputStream input = this.getClass().getResourceAsStream(path);
-
-		if (input == null) {
-			throw new IllegalStateException("Could not load expected output from path: " + path);
-		}
-
-		String expectedOutput = "";
-
-		Scanner scanner = null;
-		try {
-			scanner = new Scanner(input, "UTF-8").useDelimiter("\\A");
-			expectedOutput = scanner.hasNext() ? scanner.next() : "";
-		} finally {
-			if (scanner != null) {
-				scanner.close();
-			}
-		}
-
-		assertFalse(producedOutput.isEmpty());
-		assertFalse(expectedOutput.isEmpty());
-
-		producedOutput = producedOutput.replaceAll("\\r", "");
-		expectedOutput = expectedOutput.replaceAll("\\r", "");
-
-		// adding newlines around the output so it becomes easier to read
-		// the error message in case of failure
-		producedOutput = "\n" + producedOutput + "\n";
-		expectedOutput = "\n" + expectedOutput + "\n";
-
-		assertEquals(producedOutput, expectedOutput);
 	}
 }
